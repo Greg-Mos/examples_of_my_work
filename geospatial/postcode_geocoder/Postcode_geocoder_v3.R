798c4cfdf7eb2b1b011af8dfdf0e8dc168950f6d
Greg Moschonas <37711301+Greg-Mos@users.noreply.github.com>
2022-09-19 16:54:16 +0100

    Add files via upload

---
+++ geospatial/postcode_geocoder/Postcode_geocoder_v3.R
@@ -0,0 +1,151 @@
+# Load required packages
+list_of_packages <- c("R6", "data.table", "stringr", "dplyr", "tibble", "docstring", "lubridate", "readr")
+
+lapply(list_of_packages, library, character.only = TRUE)
+
+PostcodeGeocoder <- R6Class("PostcodeGeocoder",
+                            public = list(
+                              #' @field nspl_csv_regex Regular expression (regex) for automatically finding  
+                              #' the National Statistics Postcode Lookup (NSPL) CSV in the current working directory (cwd)
+                              nspl_csv_regex = "^(NSPL_).*20[2-9][0-9]_UK\\.csv$",
+                              #' @field pcd_extract_regex Postcode extraction regex. Tested on the NSPL data. 
+                              #' Achieves 100% extraction success for valid postcodes in NSPL columns "pcd" and "pcds".
+                              pcd_extract_regex = "[A-Za-z]{1,2}[0-9][A-Za-z0-9]? {0,2}[0-9][A-Za-z]{2}",
+                              #' @field postcodes Holds the NPSL data after initialisation, so 
+                              #' subsequent geocodings won't require reading in the NSPL CSV again. 
+                              postcodes = NULL,
+                              #' @field nspl_cols The columns that will be read in from the NSPL CSV. 
+                              nspl_cols = c("pcds","lat", "long"),
+                              initialize = function(nspl_csv = NULL, add_nspl_cols = NULL){
+                                #' @description  PostcodeGeocoder takes UK postcodes and returns the corresponding
+                                #' geographic coordinates as eastings and northings. It uses NSPL data. 
+                                #' @param nspl_csv character. The file path of the National Statistics Postcode Lookup CSV file.
+                                #' Defaults to NULL, whereby it searches the cwd for filenames starting with "NSPL_" 
+                                #' and ending with "20**_UK.csv". If multiple NSPL files are found, it uses that with the most
+                                #' recent date in its name.If none, are found, it brings up a file selection menu.
+                                #' @param add_nspl_cols character. Additional columns to import from NSPL.                                
+                                #' @examples
+                                #' post <- PostcodeGeocoder$new()
+                                #' post <- PostcodeGeocoder$new(nspl_csv = "~/GIS/Postcodes/Data/NSPL_NOV_2021_UK.csv")
+                                self$nspl_cols <- unique(c(self$nspl_cols, add_nspl_cols))
+                                private$read_nspl(nspl_csv = nspl_csv)
+                              },
+                              extract_pcds = function(column, pcd_data = NULL, ...){
+                                #' @description  Extracts postcodes from a specified postcode/address column in a CSV or dataframe 
+                                #' @param column character
+                                #' The name of the postcode/address column 
+                                #' @param pcd_data character or data.frame or tibble
+                                #' The file path of the CSV file or a data frame or a tibble with postcodes/addresses
+                                #' Defaults to NULL, whereby it brings up a file selection menu. 
+                                #' @return A tibble with the extracted postcodes in a new column "ext_pcds"
+                                #' @examples
+                                #' post <- PostcodeGeocoder$new()
+                                #' results <- post$extract_pcds("Postcode")
+                                #' results <- post$extract_pcds("postcodes", df)
+                                #' results <- post$extract_pcds("Addresses", "~/GIS/Postcodes/data.csv")
+                                tb <- private$get_data(pcd_data, ...)
+                                extracted_pcds <- stringr::str_extract(tb[[column]], self$pcd_extract_regex)
+                                tb <- tibble::add_column(tb, ext_pcds = extracted_pcds)
+                                stringr::str_c("Extracted ", sum(!is.na(tb$ext_pcds)), " out of ", length(tb$ext_pcds), " postcodes") %>% message()
+                                return(tb)
+                              },
+                              geocode_pcds = function(column, pcd_data = NULL, ...){
+                                #' @description  Geocodes postcodes, after extracting them from a specified postcode/address column 
+                                #' in a CSV or dataframe or tibble
+                                #' @param column character
+                                #' The name of the postcode/address column 
+                                #' @param pcd_data character or data.frame or tibble
+                                #' The file path of the CSV file or a data frame or a tibble with postcodes/addresses
+                                #' Defaults to NULL, whreby it brings up a file selection menu.
+                                #' @return A geocoded dataset
+                                #' @examples
+                                #' post <- PostcodeGeocoder$new()
+                                #' results <- post$geocode_pcds("Postcode")
+                                #' results <- post$geocode_pcds("postcodes", df)
+                                #' results <- post$geocode_pcds("Addresses", "~/GIS/Postcodes/data.csv")
+                                
+                                # extract
+                                tb <- self$extract_pcds(pcd_data = pcd_data, column = column, ...) %>%
+                                  dplyr::mutate(temp_pcd_key = stringr::str_to_upper(stringr::str_replace_all(ext_pcds, " ", "")))
+
+                                
+                                #filter
+                                subpost <- self$postcodes %>% 
+                                  dplyr::mutate(temp_pcd_key = stringr::str_to_upper(stringr::str_replace_all(pcds, " ", ""))) %>%
+                                  dplyr::filter(temp_pcd_key %in% tb$temp_pcd_key) 
+                                
+                                #join
+                                tb_geo <- dplyr::left_join(x = tb, y = subpost, by = "temp_pcd_key") %>% 
+                                  dplyr::select(-temp_pcd_key) %>% tibble::as_tibble()
+                                
+                                stringr::str_c("Geocoded  ", sum(!is.na(tb_geo$lat)), " out of ", sum(!is.na(tb$ext_pcds)), " postcodes") %>% message()
+                                
+                                return(tb_geo) 
+                              }
+                            ),
+                            private = list(
+                              get_data = function(pcd_data = NULL, ...){
+                                #' @description  Reads in data. Internal method for reading in data
+                                #' @param pcd_data character or data.frame or tibble
+                                #' A CSV filepath or a dataframe or a tibble. Defaults to NULL, whereby it brings up a file selection
+                                #' menu.
+                                #' @return A tibble
+                                if (is.null(pcd_data)){
+                                  file <- choose.files(multi = FALSE, 
+                                                                   caption = "Select data csv",
+                                                                   filters = matrix(c("Comma-separated values (*.csv)", "*.csv")))
+                                  tb <- data.table::fread(file = file, ...)
+                                } else if (! is.data.frame(pcd_data)){
+                                  tb <- data.table::fread(file = pcd_data, ...) 
+                                } else{
+                                  tb <- pcd_data
+                                }
+                                tb <- tibble::as_tibble(tb)
+                                return(tb)
+                              },
+                              read_nspl = function(nspl_csv = NULL){
+                                #' @description  Internal method that is used in initialisation
+                                #' @param nspl_csv character. The file path of the National Statistics Postcode Lookup CSV file.
+                                #' Defaults to NULL, whereby, it searches the cwd for filenames starting with "NSPL_" 
+                                #' and ending with "20**_UK.csv". If multiple NSPL files are found, it uses that with the most
+                                #' recent date in its name. If none are found, it brings up a file selection menu.
+                                
+                                import_nspl <- function(nspl_path){
+                                  self$postcodes <- data.table::fread(file = nspl_path, select = self$nspl_cols) %>% 
+                                    tibble::as_tibble()
+                                }
+                
+                                if (is.null(nspl_csv)){
+                                  nspl_files <- dir(pattern=self$nspl_csv_regex) 
+                                  nspl_dates <- nspl_files %>% stringr::str_sub(6, 13) %>% lubridate::my()
+                                  nspl_path <- nspl_files[nspl_dates == max(nspl_dates)]
+                                } else {
+                                  nspl_path <- nspl_csv
+                                }
+                                
+                                # allow the user to choose file interactively if the csv in not in cwd or specified path.
+                                tryCatch(
+                                  {
+                                    import_nspl(nspl_path)
+                                    message(c("Imported ", nspl_path))
+                                  },
+                                  error=function(cond){
+                                    message("NSPL csv could not be found")
+                                    message("Select file interactively")
+                                    nspl_path <- choose.files(multi = FALSE, 
+                                                              caption = "Select NSPL csv",
+                                                              filters = matrix(c("Comma-separated values (*.csv)", "*.csv")))
+                                    
+                                    import_nspl(nspl_path)
+                                    
+                                    message(c("Imported ", nspl_path))
+                                  }
+                                )
+                                
+                  
+                                self$postcodes$pcds <- stringr::str_trim(self$postcodes$pcds, side = "both")
+                              }
+                            )
+)
+
+
